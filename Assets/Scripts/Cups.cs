ed9b52644e7007d8a66220eb31976dbc9f88129e
Omar Toson Khater <44278568+OmarToson@users.noreply.github.com>
2018-10-19 04:39:28 +0200

    Edit

---
+++ Assets/Scripts/Cups.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cups : MonoBehaviour {
+
+    public float downHeight = 1.19f;
+    public float upHeight = 1.4f;
+
+    public Vector3 targetPosition;
+
+    public float speed = 5f;
+
+    public GameObject ball;
+
+    // Use this for initialization
+    void Start () {
+        // MoveUp();   //delete it because GameController control to it
+
+        targetPosition = transform.position;   //عشان فى البداية تبدا من فوق الطربيزة مش من تحتها 
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime* speed);
+	}
+
+    public void MoveUp ()
+    {
+        targetPosition = new Vector3(
+            transform.position.x,
+            upHeight,
+            transform.position.z
+         );
+    }
+
+    public void MoveDown()
+    {
+        targetPosition = new Vector3(
+            transform.position.x,
+            downHeight,
+            transform.position.z
+         );
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject ball;
+    public Cups[] cups;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(ShuffleRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator ShuffleRoutine() //دالة لانتظار بضع ثوانى
+    {
+        yield return new WaitForSeconds(1f);  //يستنى ثانية بعدين الكابس تطلع 
+        
+        foreach (Cups cup in cups)  //عشان تتعمل على ال 3 كابس
+        {
+            cup.MoveUp();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Cups targetCup = cups[Random.Range(0,cups.Length)];  //for taking random cup 
+
+        targetCup.ball = ball;
+
+        ball.transform.position = new Vector3(  //make ball go to the random cup in x,z and the same y
+            targetCup.transform.position.x,
+            ball.transform.position.y,
+            targetCup.transform.position.z
+        );
+
+        yield return new WaitForSeconds(1f);
+
+        foreach (Cups cup in cups)   //تنزل الكابس
+        {
+            cup.MoveDown();
+        }
+    }
+}
